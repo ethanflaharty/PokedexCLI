76c3316149f56ad64badd73494b63f440b05a893
Ethan Flaharty <ethanflahartybusiness@gmail.com>
2026-04-22 20:51:06 -0500

    2. Completed Help and Exit

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print(" Pokedex >")
-		scanner.Scan()
-		input := scanner.Text()
-		lowerInput := strings.ToLower(input)
-		words := strings.Fields(lowerInput)
-		fmt.Printf("Your command was: %v\n", words[0])
-	}
+	startRepl()
 }
